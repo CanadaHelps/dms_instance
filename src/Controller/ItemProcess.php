f25280e913a151d6fbc44cd491ddfe5113487747
Seamus Lee <seamus.lee@jmaconsulting.biz>
2021-07-15 04:24:44 -0400

    Fixes following e2e testing

--- src/Controller/ItemProcess.php
+++ src/Controller/ItemProcess.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\dms_instance\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Drupal\Core\Database\Database;
 use Drupal\dms_instance\Queue\DMSInstanceQueue;
 use Drupal\Core\Controller\ControllerBase;
@@ -10,12 +11,16 @@ class ItemProcess extends ControllerBase {
 
 
   public function processJobReturn() {
-    $item_id = \Drupal::routeMatch()->getParameter('item_id');
-    $aegir_instance = \Drupal::routeMatch()->getParameter('aegir_instance');
+    $item_id = \Drupal::request()->query->get('job_id');
+    $aegir_instance = \Drupal::request()->query->get('aegir_instance');
     $queue = new DMSInstanceQueue($aegir_instance, Database::getConnection());
     $item = new \stdClass();
     $item->item_id = $item_id;
     $queue->deleteItem($item);
+    return new JsonResponse([
+      'data' => ['item deleted ' . $item_id],
+      'method' => 'GET',
+    ]);
   }
 
 }
